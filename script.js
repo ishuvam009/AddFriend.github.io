058e71b07e061e0a71c7e476008852471e8abaa0
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2023-08-30 23:47:32 +0530

    Final Commit

--- script.js
+++ script.js
@@ -2,14 +2,37 @@ var istatus = document.querySelector("h5");
 
 var addFriend = document.querySelector("#add");
 
-var removeFriend = document.querySelector("#remove");
+var check = 0;
+
+// var removeFriend = document.querySelector("#remove");
 
 addFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Friends";
-    istatus.style.color = "green";
+
+    if(check == 0){
+        istatus.innerHTML = "Friends";
+        istatus.style.color = "green";
+        addFriend.innerHTML = "Remove Friend";
+        check = 1;
+    }
+
+    else{
+        istatus.innerHTML = "Strangers";
+        istatus.style.color = "red";
+        addFriend.innerHTML = "Add Friend";
+        check = 0;
+    }
+
+
+
+    
 })
 
-removeFriend.addEventListener("click", function(){
-    istatus.innerHTML = "Strangers";
-    istatus.style.color = "red";
-})
+
+
+
+
+
+// removeFriend.addEventListener("click", function(){
+//     istatus.innerHTML = "Strangers";
+//     istatus.style.color = "red";
+// })
